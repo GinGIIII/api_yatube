db762a79915c7c21366aa78a6adfbbeb8d352a7c
R1ckkkkkkkkkk <146040392+GinGIIII@users.noreply.github.com>
2026-05-03 15:30:02 +1000

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,19 +3,6 @@ from rest_framework import serializers
 from posts.models import Comment, Group, Post
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        slug_field='username',
-        read_only=True,
-    )
-    post = serializers.PrimaryKeyRelatedField(read_only=True)
-
-    class Meta:
-        model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created')
-        read_only_fields = ('author', 'post', 'created')
-
-
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username',
@@ -28,28 +15,20 @@ class PostSerializer(serializers.ModelSerializer):
         read_only_fields = ('author', 'pub_date')
 
 
-class PostWithCommentsSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        slug_field='username',
-        read_only=True,
-    )
-    comments = CommentSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Post
-        fields = (
-            'id',
-            'text',
-            'author',
-            'image',
-            'group',
-            'pub_date',
-            'comments',
-        )
-        read_only_fields = ('author', 'pub_date')
-
-
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('id', 'title', 'slug', 'description')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
+        read_only_fields = ('author', 'post', 'created')
